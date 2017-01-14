556889ea0c84e929d4e370c0115d3fdfd8d55b97
judge2020 <hunterray55@hotmail.com>
2017-01-13 22:20:18 -0500

    untested

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@
 
 import config
 import praw
-import time #need more time
-from urllib.request import urlopen
+import time  # need more time
+import urllib.request
 
 reddit = praw.Reddit(client_id=config.reddit_client_id,
                      client_secret=config.reddit_client_secret,
@@ -15,9 +15,27 @@ reddit = praw.Reddit(client_id=config.reddit_client_id,
 print('Trying to log in...')
 print('Logged in as:' + reddit.user.me())
 
+class request:
+    Hearthstone = 'wtcg'
+    Overwatch = 'Pro'
+    Starcraft2 = 'sc2'
+    Warcraft = 'WoW'
+    Diablo = 'd3'
+    HOTS = 'heroes'
+    def get_patchnotes(self, game):
+        print('Getting patchnotes for ' + game + ' in language english')
+        url = 'https://us.battle.net/connect/en/app/%s/patch-notes?productType=%s', game, game
+
+        headers = {'User-Agent': 'Battle.net/1.0.8.4217'}
+        req = urllib.Request('www.example.com', None, headers)
+        html = urllib.urlopen(req).read()
+        print(html)
+
 def main_timer():
     print('handle timer')
 
 while True:
-    main_timer()
+    request.get_patchnotes('live')
     time.sleep(config.timerInterval)
+
+
