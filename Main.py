e2b45a4e1252afc40c4073a41cbcba4fa66c8c52
judge2020 <hunterray55@hotmail.com>
2017-01-14 11:12:42 -0500

    progress2

--- Main.py
+++ Main.py
@@ -5,6 +5,8 @@ import config
 import praw
 import time  # need more time
 import urllib.request
+from bs4 import BeautifulSoup
+import html2text
 
 reddit = praw.Reddit(client_id=config.reddit_client_id,
                      client_secret=config.reddit_client_secret,
@@ -24,23 +26,30 @@ class Handler:
     Diablo = 'd3'
     HOTS = 'heroes'
 
-    def get_patchnotes(self, game):
+    def get_patchnotes_raw(game):
         print('Getting patch notes for ' + game + ' in language english')
         url = 'https://us.battle.net/connect/en/app/' + game + '/patch-notes?productType=' + game
 
         headers = {'User-Agent': 'Battle.net/1.0.8.4217'}
         req = urllib.request.Request(url, None, headers)
         html = urllib.request.urlopen(req).read()
-        print(self.parse(str))
+        return html
 
+    def markupify(str):
+        return html2text.html2text(str)
 
-    def parse(str):
-        words = str.split()
-        for word in words:
-            word.replace(r'\r\n', '')
-            word.replace(r'\xe2\x80\x99', "'")
+    def get_patch_notes_maincontent(str):
+        mySoup = BeautifulSoup(str, 'html.parser')
+        return mySoup.find('div', {"class":"patch-notes-interior"})
 
-        return words
+    def fix(byte):
+        return byte.decode('utf8')
+
+    def get_patchnotes_md(self, game):
+        raw = self.get_patchnotes_raw(game)
+        fixed = self.fix(raw) + '</script>'
+        mc = self.get_patch_notes_maincontent(raw)
+        return self.markupify(mc)
 
 
 def main_timer():
@@ -48,6 +57,5 @@ def main_timer():
 
 
 while True:
-    #Handler.get_patchnotes(Handler.Hearthstone)
-    Handler.parse(r'<p><strong>Hearthstone Update \xe2\x80\x93 11/29/16</strong></p>\r\n<p><strong>Patch 7.0.0.15590 </strong></p>\r\n<p></p>\r\n<p>The Tavern\xe2\x80\x99s never been more rough and tumble! This Hearthstone update paves the way for the arrival of The Mean Streets of Gadgetzan, Hearthstone\xe2\x80\x99s latest expansion. Join three crime families as they vie for power in the back alleys of Gadgetzan with all-new Tri-Class cards, and a gritty game board featuring each of the city\xe2\x80\x99s districts. This update also adds several swanky new card backs and fixes some bugs.</p>\r\n<p></p>\r\n<p>Read on for details!</p>\r\n<p></p>\r\n<p><strong>The Mean Streets of Gadgetzan</strong></p>\r\n<p></p>\r\n<ul>\r\n<li>Grab your shiniest brass knuckles and get ready to rumble on The Mean Streets of Gadgetzan. Hearthstone\xe2\x80\x99s latest expansion will be available beginning December 1<sup>st</sup> and includes:\r\n<ul style="list-style-type:circle;">\r\n<li>132 all-new cards.</li>\r\n<li>Tri-Class cards - special neutral minions that break the rules to be included in the decks of all three classes associated with each family:\r\n')
+    print(Handler.get_patchnotes_md(Handler, Handler.Hearthstone))
     time.sleep(config.timerInterval)
