1875fe8a36192af607234787d04636b2642f20f6
judge2020 <hunterray55@hotmail.com>
2017-01-23 10:03:33 -0500

    commen t out

--- Main.py
+++ Main.py
@@ -90,7 +90,7 @@ class timerHandler:
             print('New update: ' + ver)
             Last_ver.write(ver)
             Last_ver.close()
-            self.post_reddit(self, 'Hearthstone', 'Hearthstone Update for ' + time.strftime("%m/%d/%Y"), MDhandler.get_patchnotes_md(MDhandler, MDhandler.Hearthstone))
+            #self.post_reddit(self, 'Hearthstone', 'Hearthstone Update for ' + time.strftime("%m/%d/%Y"), MDhandler.get_patchnotes_md(MDhandler, MDhandler.Hearthstone))
 
         return
 
