7dd00e57aa0d25546869c0b99ee862d5f3c8d023
judge2020 <hunterray55@hotmail.com>
2017-01-25 13:29:24 -0500

    idk

--- test_bnetupdate.py
+++ test_bnetupdate.py
@@ -8,3 +8,5 @@ class testClass(unittest.TestCase):
 
     def test_bytefix(self):
         assert Main.MDhandler.fix(b'<p><strong>Hearthstone Update \xe2\x80\x93 11/29/16</strong></p>') == '<p><strong>Hearthstone Update \u2013 11/29/16</strong></p>'
+
+
