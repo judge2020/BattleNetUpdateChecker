98f8d147065f7943495116ec568be4361b7fa49d
judge2020 <hunterray55@hotmail.com>
2017-01-26 06:57:21 -0500

    fix tests

--- test_bnetupdate.py
+++ test_bnetupdate.py
@@ -4,9 +4,9 @@ import unittest
 
 class testClass(unittest.TestCase):
     def test_markdownify(self):
-        assert Main.MDhandler.markupify('<p><strong>Hearthstone Update \u2013 11/29/16</strong></p>') == '**Hearthstone Update \u2013 11/29/16**\r\n'
+        assert Main.MDhandler.markupify('<strong>Hearthstone 11/29/16</strong>') == '**Hearthstone 11/29/16**\r\n'
 
     def test_bytefix(self):
-        assert Main.MDhandler.fix(b'<p><strong>Hearthstone Update \xe2\x80\x93 11/29/16</strong></p>') == '<p><strong>Hearthstone Update \u2013 11/29/16</strong></p>'
+        assert Main.MDhandler.fix(b'<p><strong>Hearthstone Update 11/29/16</strong></p>') == '<p><strong>Hearthstone Update 11/29/16</strong></p>'
 
 
