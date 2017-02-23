e7c4dd4296036e5864e4bfc92e564b0fb66fdab1
judge2020 <hunterray55@hotmail.com>
2017-02-23 15:57:17 -0500

    fix stdout

--- Main.py
+++ Main.py
@@ -132,7 +132,7 @@ class timerHandler:
 
     def mainTimer(self):
         try:
-            sys.stdout = open("BattleNetUpdateChecker.log", "w")
+            sys.stdout = open("BattleNetUpdateChecker.log", "rw")
             if self.interv == 1:
                 self.CheckHS(self)
                 self.interv += 1
